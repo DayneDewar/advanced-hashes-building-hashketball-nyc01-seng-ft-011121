32909c26770b8e58349dd5a7a0d62a0a73624aff
Dayne Dewar <daynedewar@yahoo.com>
2021-01-02 19:13:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,14 +5,15 @@ def game_hash
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
-      {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
+      {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1}, 
+      {player_name:, number:, shoe:, points:, rebounds:, assists:, steals:, blocks:, slam_dunks:}  
       ]
   }
   binding.pry
   hash[:away] = {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
-    :players => []
+    :players => [{player_name:, number:, shoe:, points:, rebounds:, assists:, steals:, blocks:, slam_dunks:} ]
   }
   hash
 end
