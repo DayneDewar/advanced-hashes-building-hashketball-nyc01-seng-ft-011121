6c788798e5b9c65581068b13cdef4d1c8eb91d44
Dayne Dewar <daynedewar@yahoo.com>
2021-01-02 19:00:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
   hash[:home] = {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => [player_name: ]
+    :players => [player_name: ""]
   }
   hash[:away] = {
     :team_name => "Charlotte Hornets",
